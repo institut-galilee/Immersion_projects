9d8e93227581c3e48462421d4a883d3ddcc6bc77
Greyman93 <40568634+Greyman93@users.noreply.github.com>
2019-03-19 13:48:51 +0100

    gestion seekbar

--- MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
@@ -17,6 +17,7 @@ public class ActivityMain2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+<<<<<<< Updated upstream
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Theme, android.R.layout.simple_spinner_item);
@@ -24,6 +25,29 @@ public class ActivityMain2 extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+=======
+
+        sr = (SeekBar) findViewById(R.id.rouge);
+        sv = (SeekBar) findViewById(R.id.vert);
+        sb = (SeekBar) findViewById(R.id.bleu);
+
+        sr.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                r = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+>>>>>>> Stashed changes
     }
 
 

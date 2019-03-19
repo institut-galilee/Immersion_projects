4cc281ac659562a4601ece0b6f9b0434033ac742
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2019-03-19 13:46:31 +0100

    Seekbar page ActivityMain2

--- MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
@@ -4,7 +4,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.Manifest;
+import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 
 public class ActivityMain2 extends AppCompatActivity {
 
@@ -15,7 +17,16 @@ public class ActivityMain2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Theme, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
+
 
 
 

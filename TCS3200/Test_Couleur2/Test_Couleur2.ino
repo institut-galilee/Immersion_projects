f7c6e58cccd2f10f74f1c66fc50b008d8a005d80
Mohaabenz <bensaad.mohamedamine@gmail.com>
2019-03-19 11:34:27 +0100

    MAJ CAPTEUR ET ACHAT LED

--- TCS3200/Test_Couleur2/Test_Couleur2.ino
+++ TCS3200/Test_Couleur2/Test_Couleur2.ino
@@ -116,13 +116,17 @@ void loop() {
     Serial.println(int(g_array[i] * g_SF[i]));
   }
   if (red > 225 && red < 260 && grn > 225 && grn < 260 && blu > 225 && blu < 260) color = "WHITE";
-  else if (red >= 0 && red <= 10 && grn >= 0 && grn <= 10 && blu >= 0 && blu <= 10) color = "BLACK";
-  else if (red > 160 && red < 260 && grn >= 0 && grn <= 50 && blu >= 0 && blu <= 50) color = "RED";
-  else if (red >=0 && red <= 70 && grn >= 142 && grn < 260 && blu >= 0 && blu < 60) color = "GREEN";
-  else if (red >= 0 && red <= 20 && grn >=0 && grn <= 70 && blu >= 150 && blu <= 260) color = "BLUE";
-  else if (red >= 220 && red <= 260 && grn >= 170 && grn <= 260 && blu >=0 && blu <= 100) color = "YELLOW";
-  else if (red >= 0 && red <= 80 && grn >= 170 && grn <= 260 && blu >=180 && blu <= 260) color = "CYAN";
-  else if (red >= 170 && red <= 260 && grn >= 0 && grn <= 100 && blu >=180 && blu <= 260) color = "MAGENTA";
+  else if (red >= 0 && red <= 15 && grn >= 0 && grn <= 15 && blu >= 0 && blu <= 15) color = "BLACK";
+  else if (red > 190 && red < 260 && grn >= 35 && grn <= 45 && blu >= 45 && blu <= 55) color = "RED";
+  else if (red >=75 && red <= 85 && grn >= 160 && grn < 260 && blu >= 60 && blu < 70) color = "GREEN";
+  else if (red >= 20 && red <= 30 && grn >=75 && grn <= 85 && blu >= 180 && blu <= 260) color = "BLUE";
+  else if (red >= 240 && red <= 255 && grn >= 195 && grn <= 210 && blu >=95 && blu <= 105) color = "YELLOW";
+  else if (red >= 85 && red <= 95 && grn >= 220 && grn <= 230 && blu >=120 && blu <= 230) color = "CYAN";
+  else if (red >= 195 && red <= 205 && grn >= 105 && grn <= 115 && blu >=215 && blu <= 230) color = "MAGENTA";
+  else if (red >= 205 && red <= 215 && grn >= 70 && grn <= 85 && blu >=65 && blu <= 80) color = "ORANGE";
+  else if (red >= 40 && red <= 50 && grn >= 25 && grn <= 35 && blu >=45 && blu <= 55) color = "PURPLE";
+  else if (red >= 20 && red <= 35 && grn >= 45 && grn <= 55 && blu >=45 && blu <= 55) color = "TURQUOISE";
+
   else  color = "NO_COLOR";
   Serial.println(color);
   delay(4000);

9becd8816d066c45101d5321f83e13887abc9438
Greyman93 <40568634+Greyman93@users.noreply.github.com>
2019-03-19 13:49:40 +0100

    gestion seekbar

--- MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
@@ -17,7 +17,6 @@ public class ActivityMain2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-<<<<<<< Updated upstream
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Theme, android.R.layout.simple_spinner_item);
@@ -25,7 +24,7 @@ public class ActivityMain2 extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-=======
+
 
         sr = (SeekBar) findViewById(R.id.rouge);
         sv = (SeekBar) findViewById(R.id.vert);
@@ -47,7 +46,7 @@ public class ActivityMain2 extends AppCompatActivity {
 
             }
         });
->>>>>>> Stashed changes
+
     }
 
 

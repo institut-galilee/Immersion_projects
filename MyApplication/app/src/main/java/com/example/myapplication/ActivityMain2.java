157c7de1122ef62a82c7c8b9fb84fc85c680438a
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2019-03-19 13:36:59 +0100

    Redirection page 2

--- MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.Manifest;
 import android.widget.SeekBar;
 
-class ActivityMain2 extends AppCompatActivity {
+public class ActivityMain2 extends AppCompatActivity {
 
     private int r, v, b;
     private SeekBar sr, sv, sb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
 
 
 

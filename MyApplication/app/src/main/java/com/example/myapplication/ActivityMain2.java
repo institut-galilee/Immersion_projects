89e5db644556581ff68060de113f6635e8cd08a0
Greyman93 <40568634+Greyman93@users.noreply.github.com>
2019-03-20 14:59:24 +0100

    Implémentation des thèmes

--- MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain2.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.Manifest;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
 import android.widget.Spinner;
@@ -11,21 +13,26 @@ import android.widget.TextView;
 
 public class ActivityMain2 extends AppCompatActivity {
 
+    //Valeur du rouge, vert et bleu respectivement
     private int r, v, b;
+    //Jauges pour la gestion du rouge, vert et bleu respactivement
     private SeekBar sr, sv, sb;
+    //Affichage du rouge, vert et bleu respectivement
     private TextView tr, tv, tb;
+    //Menu des themes
+    private Spinner themes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        themes = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Theme, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        themes.setAdapter(adapter);
 
 
         sr = (SeekBar) findViewById(R.id.rouge);
@@ -35,12 +42,39 @@ public class ActivityMain2 extends AppCompatActivity {
         tr = (TextView) findViewById(R.id.tr);
         tv = (TextView) findViewById(R.id.tv);
         tb = (TextView) findViewById(R.id.tb);
+        tr.setText(""+r+"");
+        tv.setText(""+v+"");
+        tb.setText(""+b+"");
 
+        themes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CharSequence themeSelectionne = (CharSequence) parent.getItemAtPosition(position);
+                String s = themeSelectionne.toString();
+                switch (s){
+                    case "Theme 1":
+                        theme1();
+                        break;
+                    case "Theme 2":
+                        theme2();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+        //Gestion de l'affichage des valeurs en instantané
         sr.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 r = progress;
-                tr.setText(r);
+                tr.setText(""+progress+"");
             }
 
             @Override
@@ -54,8 +88,66 @@ public class ActivityMain2 extends AppCompatActivity {
             }
         });
 
+        sv.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                v = progress;
+                tv.setText(""+progress+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                b = progress;
+                tb.setText(""+progress+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        /*r = sr.getProgress();
+        v = sv.getProgress();
+        b = sb.getProgress()*/
+
+
     }
 
+    public void theme1(){
+        this.r = 44;
+        this.v = 120;
+        this.b = 78;
+        sr.setProgress(this.r);
+        sv.setProgress(this.v);
+        sb.setProgress(this.b);
+    }
+
+    public void theme2(){
+        this.r = 112;
+        this.v = 0;
+        this.b = 255;
+        sr.setProgress(this.r);
+        sv.setProgress(this.v);
+        sb.setProgress(this.b);
+    }
 
 
 

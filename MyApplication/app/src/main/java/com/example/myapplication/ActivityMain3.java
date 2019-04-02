c16422236f7689bac68a80d24815f29273ee809d
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2019-04-02 13:56:57 +0200

    MAJ

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain3.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+import android.app.AppComponentFactory;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityMain3 extends AppCompatActivity {
+}

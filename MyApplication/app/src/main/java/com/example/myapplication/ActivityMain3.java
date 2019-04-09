f16f9a4aad6a960841450b04541502735b700c6c
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2019-04-09 17:09:05 +0200

    Merge branch 'master' of C:\Users\Heba\Documents\GitHub\Immersion_projects with conflicts.

--- MyApplication/app/src/main/java/com/example/myapplication/ActivityMain3.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ActivityMain3.java
@@ -4,4 +4,21 @@ import android.app.AppComponentFactory;
 import android.support.v7.app.AppCompatActivity;
 
 public class ActivityMain3 extends AppCompatActivity {
+
+
+
+
+    /*
+
+    @Test
+    public void getInputStreamFeeder() throws Exception {
+        shadowOf(bluetoothSocket).getInputStreamFeeder().write(DATA);
+        InputStream inputStream = bluetoothSocket.getInputStream();
+        byte[] b = new byte[1024];
+        int len = inputStream.read(b);
+        assertThat(Arrays.copyOf(b, len)).isEqualTo(DATA);
+    }
+     */
+
+
 }
